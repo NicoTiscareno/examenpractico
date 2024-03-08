b8309ae2451a11a86d85e09e0d98061519575527
NicolasTiscareno <utm22030683@utnma.edu.mx>
2024-03-08 15:30:20 -0600

    fist commit

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json({limit:'50mb'}));
+app.use(express.urlencoded({limit:'50mb', extended: true }));
+
+app.listen(4000, ()=>{
+    console.log("Nicolas Gloria Tiscareño")
+});
